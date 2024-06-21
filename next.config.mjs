0f11bf4357070b349051144485b931c74c95809d
Kubilay-web <69966389+Kubilay-web@users.noreply.github.com>
2024-06-21 12:52:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+// next.config.mjs
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Convert import.meta.url to __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "./base.scss";`,
+  },
+};
+
+export default nextConfig;
